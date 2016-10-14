eb013710c818c48e9f76c57a357640cfa5fb6f87
ShiWenChen <cswzmg@live.com>
2016-10-14 12:25:56 +0800

    Initial Commit

---
+++ 心跳曲线/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  心跳曲线
+//
+//  Created by admin on 2016/10/14.
+//  Copyright © 2016年 racer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 心跳曲线/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  心跳曲线
+//
+//  Created by admin on 2016/10/14.
+//  Copyright © 2016年 racer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
